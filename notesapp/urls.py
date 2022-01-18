a28e3aeb1048e59d2354302757a5b51aa24b297e
Sherzod <thisisotajonov@gmail.com>
2022-01-18 15:38:24 +0100

    read notes

--- notesapp/urls.py
+++ notesapp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("note/<str:pk>", views.note, name="note"),
 ]
--- notesapp/views.py
+++ notesapp/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from .models import Note
 
-# Create your views here.
 def home(request):
-    return render(request, 'notesapp/main.html') 
+    context = {
+        "notes": Note.objects.all(),
+    }
+    return render(request, 'notesapp/main.html', context)
+
+def note(request, pk):
+    note = Note.objects.get(id=pk)
+    context = {
+        "note": note,
+    }
+    return render(request, 'notesapp/note.html', context)
